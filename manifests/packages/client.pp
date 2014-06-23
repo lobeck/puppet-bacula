a8551371b596f6dc9d95c8aa317ba49de4afdcf6
Christian Becker <christian@dabecka.de>
2014-06-23 22:37:08 +0200

    added bacula::packages::client

---
+++ manifests/packages/client.pp
@@ -0,0 +1,40 @@
+# == Class: bacula::packages::client
+#
+# Basic class to manage the package installation of the bacula client (bacula-fd / FileDaemon)
+#
+# Note: Unless otherwise noticed, it is recommended to use hiera for configuration
+#
+# === Parameters
+#
+# [*package_name*]
+#   Name of the bacula client package (default: bacula-client)
+#   bacula::packages::client::package_name: bacula-client
+#
+# [*package_ensure*]
+#   Package version to install (default: installed)
+#   bacula::packages::client::package_ensure: 5.2.13-1.el6
+#
+# === Variables
+#
+# === Examples
+#
+#  class { 'bacula::packages::client':
+#      package_name   => 'bacula-client',
+#      package_ensure => 'installed',
+#  }
+#
+# === Copyright
+#
+# This Code is part of puppet-bacula (https://github.com/lobeck/puppet-bacula)
+# See LICENSE.md)
+#
+class bacula::packages::client ($package_name='bacula-client', $package_ensure='installed') {
+
+    package { 'bacula-client':
+        ensure => $package_ensure,
+        name   => $package_name,
+    }
+
+}
+
+#:vim set ts=4 et
